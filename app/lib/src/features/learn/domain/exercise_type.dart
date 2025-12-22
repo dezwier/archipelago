f2e6c0e866a04217c29b7ec833a882796dac4d5b
Desiré De Waele <desiredewaele@shurgard.eu>
2025-12-22 11:45:56 +0100

    Update ExerciseType display names to reflect new versioning scheme
    - Changed display names for various exercise types to follow a consistent versioning format, enhancing clarity and organization.
    - Adjusted the numbering system to improve the hierarchical structure of exercise types, ensuring better user understanding.

--- app/lib/src/features/learn/domain/exercise_type.dart
+++ app/lib/src/features/learn/domain/exercise_type.dart
@@ -15,26 +15,25 @@ enum ExerciseType {
   String get displayName {
     switch (this) {
       case ExerciseType.discovery:
-        return '1.1 Discovery';
+        return '0.1 Discovery';
       case ExerciseType.summary:
-        return '1.2 Discovery Summary';
+        return '0.2 Discovery Summary';
       case ExerciseType.matchInfoImage:
-        return '2.1 Match Info to Image';
+        return '1.1 Match Info to Image';
       case ExerciseType.matchImageInfo:
-        return '2.2 Match Image to Info';
+        return '1.2 Match Image to Info';
       case ExerciseType.matchAudioImage:
-        return '2.3 Match Audio to Image';
+        return '1.3 Match Audio to Image';
       case ExerciseType.matchImageAudio:
-        return '2.4 Match Image to Audio';
+        return '1.4 Match Image to Audio';
       case ExerciseType.matchDescriptionPhrase:
-        return '2.5 Match Description to Phrase';
+        return '1.5 Match Description to Phrase';
       case ExerciseType.matchPhraseDescription:
-        return '2.6 Match Phrase to Description';
+        return '1.6 Match Phrase to Description';
       case ExerciseType.scaffoldFromImage:
-        return '3.1 Scaffold From Image';
+        return '2.1 Scaffold From Image';
       case ExerciseType.closeExercise:
-        return '4.1 Close Exercise';
+        return '3.1 Close Exercise';
     }
   }
 }
-
